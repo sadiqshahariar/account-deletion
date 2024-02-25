566cf8130b6ca5a70af2010dc7e4a0810fc6c904
Sadiq <62433633+sadiqshahariar@users.noreply.github.com>
2024-02-25 10:06:57 +0600

    Add files via upload

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,79 @@
+﻿using AccountDeletion.Models;
+using AccountDeletion.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Principal;
+
+namespace AccountDeletion.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public DashboardController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult app_config()
+        {
+            List<AppConfig> appconfig= _accountRepository.GetData();
+
+            return View(appconfig);
+        }
+        public IActionResult AddConfing()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DeleteConfing(int id)
+        {
+            //int ans = id;
+            bool deleteconfig = _accountRepository.DeleteConfingData(id);
+            return RedirectToAction("app_config", "Dashboard");
+        }
+        
+        public IActionResult UpdateConfing(int id)
+        {
+            return View(id);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddConfingData(string appid, string bannercolor, string formcolor, string logo, string inputlevel, string phonenumber, string buttoncolor)
+        {
+            //var successMessage = "this from not add fully all data";
+            try
+            {
+
+                int ans = await _accountRepository.SaveConfing(appid, bannercolor, formcolor, logo, inputlevel, phonenumber, buttoncolor);
+                // successMessage = "User Details Add Successfully.";
+
+            }
+            catch (Exception ex) { }
+
+            return RedirectToAction("app_config","Dashboard");
+
+        }
+
+        public async Task<IActionResult> UpdateConfingData(int id, string bannercolor, string formcolor, string logo, string inputlevel, string phonenumber, string buttoncolor)
+        {
+
+            string successMessage;
+            try
+            {
+                // Find the user by userId
+                bool isExistid = await _accountRepository.Updateconfingdata(id, bannercolor, formcolor, logo, inputlevel, phonenumber, buttoncolor);
+
+
+                if (isExistid)
+                {
+                    successMessage = "User details updated successfully.";
+                }
+            }
+            catch(Exception ex) { }
+
+            return RedirectToAction("app_config", "Dashboard");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,71 @@
+﻿using AccountDeletion.Models;
+using AccountDeletion.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AccountDeletion.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+
+        public HomeController(IAccountRepository accountRepository)
+        {
+            _accountRepository = accountRepository;
+        }
+        public async Task<IActionResult> Index(string appid, string message)
+        {
+            var app = await _accountRepository.IsIdExist(appid);
+
+            if (app == null)
+            {
+                return NotFound();
+            }
+            var appConfig = await _accountRepository.GetAppConfig(app.Id);
+            ViewBag.SuccessMessage = message;
+            return View(appConfig);
+        }
+
+        public async Task<IActionResult> SubmitForm(string appid, string data)
+        {
+          
+            await _accountRepository.SaveUserDetails(appid, data);
+  
+            // get app id
+            var appInfo = await _accountRepository.GetAppById(appid);
+            var successMessage = "Your account deletion request received successfully";
+            return RedirectToAction("Index", new { appid = appInfo.AppId, message = successMessage });
+        }
+
+        [HttpPost]
+        public IActionResult LogIn(string username, string password)
+        {
+            //var successMessage = "";
+
+
+            int ans = _accountRepository.LoginUser(username, password);
+            if (ans<0)
+            {
+                //successMessage = "LoginFaild";
+                return RedirectToAction("Login", "Home");
+            }
+            return RedirectToAction("Index", "Dashboard");
+
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    //url:http://localhost:5155/?appid=123456789
+}
